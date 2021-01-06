7dfc57eef5532aab175bed4a8110116b0bc38fb6
Jaehee Ryoo <jaehee.ryoo@ons.gov.uk>
2021-01-06 15:05:08 +0000

    deleted setwd line

--- scripts/export_pivot_table_raw.R
+++ scripts/export_pivot_table_raw.R
@@ -7,7 +7,7 @@ library(rpivotTable)
 
 mypivot = rpivotTable(data,width="100%", height="400px")
 
-setwd("~/D:/_BPI/CARS_output")
+
 
 library(htmlwidgets)
 saveWidget(mypivot, file=paste("D:/_BPI/CARS_output/CARS wave 2 pivot table - ", format(Sys.time(),'%Y-%m-%d %H.%M.%S %p %Z'), ".html", sep=""))
